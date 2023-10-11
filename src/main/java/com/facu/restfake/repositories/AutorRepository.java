9e1d51a43c89ae99c20f12c457db083287bc3598
ValentinDMartinez <valentindantemartinez@gmail.com>
2023-10-11 18:33:17 -0300

    a

--- src/main/java/com/facu/restfake/repositories/AutorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.facu.restfake.repositories;
-
-import com.facu.restfake.entities.Autor;
-import com.facu.restfake.entities.Persona;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AutorRepository extends BaseRepository<Autor, Long> {
-}
--- src/main/java/com/facu/restfake/services/LocalidadService.java
+++
@@ -1,8 +0,0 @@
-package com.facu.restfake.services;
-
-import com.facu.restfake.entities.Localidad;
-import com.facu.restfake.entities.Persona;
-
-public interface LocalidadService extends BaseService<Localidad, Long> {
-
-}
